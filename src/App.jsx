ef599dacf17157fcaf991bf8bb8a2dd8048cd706
Jaron Proehl <jaron@strategicmissionelements.com>
2023-12-20 21:18:38 -0500

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
   
-
+console.log(puppyList);
   return (
     <>
      
