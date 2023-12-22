f84504603fedb254520ceb2f7e599a6f0d997aeb
Jaron Proehl <jaron@strategicmissionelements.com>
2023-12-21 22:06:45 -0500

    finished assignment

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,34 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 import {puppyList} from './data.js'
 import './App.css'
+import './index.css'
 
 function App() {
-const [puppies, setPuppies] = useState(puppyList) 
+const [puppies, setPuppies] = useState(puppyList);
+const [featPupId, setFeatPupId] = useState(null);
+
 console.log(puppies);
+
+const featuredPup = puppies.find((pup) => pup.id === featPupId)
+console.log(featuredPup);
   return (
     <div>
      {
       puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
       })
-     }
+     } 
+    {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default App
@@ -44,4 +59,8 @@ export default App
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+*/
+
+/*
+ <p>{ featPupId && <p>{ featPupId }</p> }</p> 
 */
